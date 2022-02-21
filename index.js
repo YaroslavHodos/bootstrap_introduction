23ef201105a46ab8eaace6b056466718959af6aa
YaroslavHodos <h0543510343@gmail.com>
2022-02-21 18:12:11 +0200

    HW#21

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-const root = document.getElementById('root');
-// root.innerHTML = `<ul>${getItems()}</ul>`
-// function range(from, length) {
-//     const res = [];
-//     for (let i = 0; i < length; i++) {
-//         res.push(i + from);
-//     }
-//     return res;
-// }
-// function getItems() {
-//     return range(1, 10).map(i => `<li> <img src="bw2.png" > item ${i.toSting()} </li>`).join('');
-// }
-let currentPage = 1;
-let pages = 30;
-const paginator = new Paginator(5, 'root', displayPage);
-function displayPage(page) {
-    if (page == 'next'){
-        currentPage = currentPage + 5;
-        if (currentPage > pages) {
-            currentPage = 1;
-        }
-        paginator.show(currentPage)
-    } else if(page == 'prev') {
-        currentPage = currentPage - 5;
-        if (currentPage < 0) {
-            currentPage = 1;
-        }
-        paginator.show(currentPage)
-    } else {
-        console.log(page)
-    }
-    
+const CHESS_PARENT_ID = "chess-parent"
+const chessBoard = new ChessBoard(CHESS_PARENT_ID, chessCell);
+chessBoard.render();
+const alertParent = document.getElementById("alert");
+function chessCell(column, row) {
+    alertParent.innerHTML = `<div class="alert alert-warning alert-dismissible" role="alert">
+    <strong>You have pressed ${column + row}</strong> 
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`
+
 }
